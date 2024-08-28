82864a429cc3bc1595f7f49450f80c3a671adf9c
MasterMagicStep <niklepa@mail.ru>
2024-08-28 14:05:25 +0300

    Add files via upload

---
+++ Методы строк.py
@@ -0,0 +1,14 @@
+#
+MyString = input('Введите текст: ')
+#
+print(MyString.upper())
+#
+print(MyString.lower())
+#
+print(MyString.replace(' ',''))
+#
+print(MyString[0])
+#
+print(MyString[-1])
+#
+print(len(MyString))
